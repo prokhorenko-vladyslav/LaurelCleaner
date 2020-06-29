f32fbee49518a2bdda91489a2340cbc5c6d3b6ae
prokhorenko-vladyslav <random.person.1907@gmail.com>
2020-06-29 23:41:15 +0300

    Added register and boot methods to CleanerServiceProvider

--- src/app/Providers/CleanerServiceProvider.php
+++ src/app/Providers/CleanerServiceProvider.php
@@ -2,7 +2,27 @@
 
 namespace Laurel\Cleaner\App\Providers;
 
-class CleanerServiceProvider
-{
+use Illuminate\Support\ServiceProvider;
 
+class CleanerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
 }
